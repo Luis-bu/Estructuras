97793690ae4c06c32c845a120eecfc002c94dd54
Luis Ernesto Bueno Solano <luisbuemaxx@gmail.com>
2025-08-26 17:07:45 +0000

    Actualizar main

--- Proyecto/Entrega 1/main.cpp
+++ Proyecto/Entrega 1/main.cpp
@@ -6,7 +6,6 @@
 using namespace std; 
 
 // Función para dividir una cadena en tokens
-// La Utilizamos para separar el comando y sus parámetros
 vector<string> dividir(const string& entrada) {
     vector<string> tokens;
     stringstream ss(entrada);
@@ -18,7 +17,6 @@ vector<string> dividir(const string& entrada) {
 }
 
 // Nuestro programa principal
-// Aquí se gestionan los comandos y se interactúa con el usuario
 int main() {
     ManejadorComandos manejador;
     string entrada;
@@ -27,7 +25,6 @@ int main() {
     while (true) {
         cout << "$ ";
         getline(cin, entrada);
-        //Usamos validación para evitar errores de entrada
         entrada.erase(0, entrada.find_first_not_of(" \n\r\t")); 
         entrada.erase(entrada.find_last_not_of(" \n\r\t") + 1); 
         if (entrada.empty()) continue;
@@ -36,20 +33,17 @@ int main() {
         string comando = tokens[0];
         vector<string> params(tokens.begin() + 1, tokens.end());
 
-        
         if (comando == "salir" && params.empty()) {
             break;  
         } else if (comando == "salir" && !params.empty()) {
             cout << "El comando 'salir' no acepta parámetros." << endl;
             continue;
-        }
-        else if (comando == "ayuda" && params.empty()) {
+        } else if (comando == "ayuda" && params.empty()) {
             manejador.mostrarAyuda();
         } else if (comando == "ayuda" && params.size() == 1) {
             manejador.mostrarAyudaComando(params[0]);
         } else if (manejador.validarComando(comando, params)) {
-            cout << "Comando " << comando << " válido, pero no implementado en nuestra entrega" << endl;
-            cout << "Procesamiento completado." << endl;
+            manejador.ejecutarComando(comando, params);
         }
     }
 
